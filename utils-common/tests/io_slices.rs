4e8d96409d80edaebd980c628641fb22f996b6d4
Oliver Steffen <osteffen@redhat.com>
2026-04-01 16:23:28 +0200

    utils-common: io_slices: Tests for DoubleEndedIoSlicesIter
    Add tests for all relevant implementations for the
    DoubleEndedIoSlicesIter trait in utils-common/src/io_slices.rs
    
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>

--- utils-common/tests/io_slices.rs
+++ utils-common/tests/io_slices.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod io_slices {
+    mod double_ended_io_slices_iter;
     mod double_ended_io_slices_mut_iter;
     mod io_slices_iter;
     mod io_slices_mut_iter;
